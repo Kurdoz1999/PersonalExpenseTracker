3856dec9c581733077484389c354ccf574b35671
Kurdoz1999 <roek0001@stud.kea.dk>
2025-04-19 15:59:05 +0200

    Reader coded successfully

--- PersonalExpenseTracker/Program.cs
+++ PersonalExpenseTracker/Program.cs
@@ -1,11 +1,42 @@
-﻿namespace PersonalExpenseTracker
+﻿using System;
+using System.IO;
+
+namespace PersonalExpenseTracker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //FileReader:
+            string data;
+            StreamReader reader = null;
 
+            try
+            {
+                reader = new StreamReader("C:\\Users\\robin\\Documents\\Zealand\\Projekter (ikke-studierelateret)\\PersonalExpenseTrackerProject.txt");
+                data = reader.ReadLine();
+
+                while (data != null)
+                {
+                    Console.WriteLine(data);
+                    data = reader.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                reader.Close();
+            }
+
+
+            //FileWriter:
+
+
+
+            Console.ReadKey();
         }
     }
 }
