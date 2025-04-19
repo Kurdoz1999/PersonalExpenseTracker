0edf0ec995cefcafe295033986f22eab6ca5a2ed
Kurdoz1999 <roek0001@stud.kea.dk>
2025-04-19 15:27:55 +0200

    Add project files.

---
+++ PersonalExpenseTracker/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalExpenseTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
